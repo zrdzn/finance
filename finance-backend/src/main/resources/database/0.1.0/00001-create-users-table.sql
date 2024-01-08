e625f8b0b202f70df389313e8d0c6d42933aee05
zrdzn <zrdzn9@gmail.com>
2024-01-08 15:36:29 +0100

    Add email to user scheme

--- finance-backend/src/main/resources/database/0.1.0/00001-create-users-table.sql
+++ finance-backend/src/main/resources/database/0.1.0/00001-create-users-table.sql
@@ -3,6 +3,7 @@
 create table "users"
 (
     "id"          serial,
+    "email"       text not null,
     "username"    text not null,
     "password"    varchar(100) not null,
     constraint "pk_users" primary key ("id")
