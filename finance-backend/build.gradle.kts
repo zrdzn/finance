feac6deef30f904d38f624074605f46a234b3ff5
zrdzn <zrdzn9@gmail.com>
2024-03-18 00:40:23 +0100

    Fix KClass related error

--- finance-backend/build.gradle.kts
+++ finance-backend/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 
     implementation("com.auth0:java-jwt:4.4.0")
 
-    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.2.41")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 java {
