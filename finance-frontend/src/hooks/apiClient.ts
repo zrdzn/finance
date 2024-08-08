73a38eacd924d4555ebe3b9f2683ef5e0179c59d
zrdzn <zrdzn9@gmail.com>
2024-08-08 12:24:30 +0200

    Adjust frontend

--- finance-frontend/src/hooks/apiClient.ts
+++ finance-frontend/src/hooks/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, {Axios} from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
