73fd9a907f51422796f6b970d1f49ed638393d6c
zrdzn <zrdzn9@gmail.com>
2024-12-30 22:16:21 +0100

    Add audit test

---
+++ finance-backend/src/integration/kotlin/dev/zrdzn/finance/backend/audit/AuditSpecification.kt
@@ -0,0 +1,25 @@
+package dev.zrdzn.finance.backend.audit
+
+import dev.zrdzn.finance.backend.audit.api.AuditAction
+import dev.zrdzn.finance.backend.audit.api.AuditResponse
+import dev.zrdzn.finance.backend.product.ProductSpecification
+
+open class AuditSpecification : ProductSpecification() {
+
+    protected val auditService: AuditService get() = application.getBean(AuditService::class.java)
+    protected val auditRepository: AuditRepository get() = application.getBean(AuditRepository::class.java)
+
+    fun createAudit(
+        userId: Int,
+        vaultId: Int,
+        description: String = "",
+        auditAction: AuditAction = AuditAction.PRODUCT_CREATED
+    ): AuditResponse =
+        auditService.createAudit(
+            userId = userId,
+            vaultId = vaultId,
+            description = description,
+            auditAction = auditAction
+        )
+
+}
