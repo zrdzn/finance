5bf9b1ca6b29fd4b5ca1acb81bc80558dd4b4b14
zrdzn <zrdzn9@gmail.com>
2025-03-20 22:11:12 +0100

    Get old logging back

--- finance-backend/src/main/kotlin/dev/zrdzn/finance/backend/configuration/SecurityConfiguration.kt
+++ finance-backend/src/main/kotlin/dev/zrdzn/finance/backend/configuration/SecurityConfiguration.kt
@@ -6,6 +6,7 @@ import java.time.Clock
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -64,6 +65,11 @@ class SecurityConfiguration(
                 AuthenticationFilter(tokenService, clock),
                 UsernamePasswordAuthenticationFilter::class.java
             )
+            .exceptionHandling {
+                it.authenticationEntryPoint { _, response, _ ->
+                    response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.reasonPhrase)
+                }
+            }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .build()
 
